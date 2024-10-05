272fde1c6ea89619e76080c26aea4419f243ed6c
josephjoeljo <joeljordanjoseph@gmail.com>
2024-10-05 11:29:03 -0400

    feat: added cors to event  api

--- event/api.go
+++ event/api.go
@@ -32,8 +32,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.Location(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("GET")
+	).Methods("GET", "OPTIONS")
 
 	// get events
 	e.Router.Handle("/v1/events",
@@ -41,8 +42,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.GetEventsByLocation(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("GET")
+	).Methods("GET", "OPTIONS")
 
 	// get events by field
 	e.Router.Handle("/v1/events/field/{id}",
@@ -50,8 +52,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.GetEventsByField(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("GET")
+	).Methods("GET", "OPTIONS")
 
 	// get an event
 	e.Router.Handle("/v1/events/{id}",
@@ -59,8 +62,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.GetEvent(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("GET")
+	).Methods("GET", "OPTIONS")
 
 	// create an event
 	e.Router.Handle("/v1/events",
@@ -68,8 +72,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.CreateEvent(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("POST")
+	).Methods("POST", "OPTIONS")
 
 	// update an event
 	e.Router.Handle("/v1/events/{id}",
@@ -77,8 +82,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.UpdateAnEvent(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("PUT")
+	).Methods("PUT", "OPTIONS")
 
 	// delete an event
 	e.Router.Handle("/v1/events/{id}",
@@ -86,8 +92,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.DeleteAnEvent(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("DELETE")
+	).Methods("DELETE", "OPTIONS")
 
 	/*
 		EVENT PARTICIPANTS
@@ -99,8 +106,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.AddParticipant(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("POST")
+	).Methods("POST", "OPTIONS")
 
 	// remove a participant
 	e.Router.Handle("/v1/events/{id}/participants/{participantID}",
@@ -108,8 +116,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.RemoveParticipant(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("DELETE")
+	).Methods("DELETE", "OPTIONS")
 
 	/*
 		EVENT NOTIFICATIONS
@@ -121,8 +130,9 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.NotifyParticipants(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("POST")
+	).Methods("POST", "OPTIONS")
 
 	// notify club members
 	e.Router.Handle("/v1/events/{id}/notify/club",
@@ -130,6 +140,7 @@ func (e *EventAPI) Ready(firebase *auth.Client) {
 			e.Service.NotifyClubMembers(),
 			middleware.Logging(),
 			middleware.UserMiddleware(firebase),
+			middleware.CORS(),
 		),
-	).Methods("POST")
+	).Methods("POST", "OPTIONS")
 }
