e2a66a0c26513e03d7dac30b13540fd131c89070
Joel Joseph <joeljordanjoseph@gmail.com>
2026-02-09 18:44:38 -0500

    support user id header on dev mode

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,6 +1,9 @@
 package middleware
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+)
 
 func CORS() Middleware {
 	return func(f http.HandlerFunc) http.HandlerFunc {
@@ -8,7 +11,16 @@ func CORS() Middleware {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+
+			allowedHeaders := "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"
+
+			// In development mode, allow the userid header for dev auth
+			mode := os.Getenv("MODE")
+			if mode != "PRODUCTION" {
+				allowedHeaders += ", userID"
+			}
+
+			w.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
 
 			if r.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusNoContent)
